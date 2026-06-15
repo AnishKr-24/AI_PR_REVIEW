d427e4ad4d1a9b6b3877785f264fe63a739ee56a
Anish Kumar <anishkumar96084@gmail.com>
2026-06-15 23:23:16 +0530

    init_shadcn-ui-tanstack-query

--- ai-code-review/app/layout.tsx
+++ ai-code-review/app/layout.tsx
@@ -3,8 +3,9 @@ import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,14 +34,16 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider
+        <QueryProvider>
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
-          {children}
+            {children}
           </ThemeProvider>
+        </QueryProvider>
         {children}</body>
     </html>
   );
